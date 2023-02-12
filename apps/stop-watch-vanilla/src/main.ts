6aef3354538aa33929f7e0678e94ad635a5f5d0c
Omar <38073753+lordsayur@users.noreply.github.com>
2023-02-12 01:33:00 +0000

    ✅ sw: implement button state logic

--- apps/stop-watch-vanilla/src/main.ts
+++ apps/stop-watch-vanilla/src/main.ts
@@ -1,23 +1,17 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import { setupCounter } from './counter'
+import "./style.css";
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+const startBtn = document.getElementById("start-btn") as HTMLButtonElement;
+const stopBtn = document.getElementById("stop-btn") as HTMLButtonElement;
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+startBtn.disabled = false;
+stopBtn.disabled = true;
+
+startBtn?.addEventListener("click", () => {
+  startBtn.disabled = true;
+  stopBtn.disabled = false;
+});
+
+stopBtn.addEventListener("click", () => {
+  stopBtn.disabled = true;
+  startBtn.disabled = false;
+});
